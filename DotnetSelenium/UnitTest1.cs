44e13f98c5f1cbca1c9bcf8e8abf3b0eb83d83c0
Njeri Muriithi <cnjeri388@gmail.com>
2025-03-17 15:14:09 +0300

    Add project files.

---
+++ DotnetSelenium/UnitTest1.cs
@@ -0,0 +1,28 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.BiDi;
+using OpenQA.Selenium.Chrome;
+
+namespace DotnetSelenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+            webElement.SendKeys("Selenium");
+            webElement.SendKeys(Keys.Return);
+
+
+            
+        }
+    }
+}
